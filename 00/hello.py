241448c2e83bed4f75e0b65c255fe757c9cf0df5
Hiyori Watabe <t22cs057@yamanashi.ac.jp>
2024-10-11 15:12:10 +0900

    練習のためコミット

---
+++ 00/hello.py
@@ -0,0 +1,23 @@
+print('Hello, Python world!')
+
+import math
+
+print(type(math))
+print(math)
+
+print(math.pi)
+
+n = 3
+for x in range(n):
+    print(x+1)
+    
+a = 10
+b = 0.000001
+c = 'string'
+
+p = 0
+while p<10:
+    print('p =',p)
+    p += 1
+
+print(a, b, c)
